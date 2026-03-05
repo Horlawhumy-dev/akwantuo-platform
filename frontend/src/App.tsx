164034c08fb3d45600b71817d89846a7ca9ba406
Arafat Olayiwola <harof.dev@gmail.com>
2026-03-05 06:28:50 +0000

    chore: frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Welcome to Akwantuo!</h1>
+    </div>
+  );
+}
+
+export default App;
